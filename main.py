9c8d52fa6d87919059d2927a9a2585e03eb45e29
ahen <iamamber0702@gmail.com>
2021-01-07 01:25:52 +0800

    Add .csv to .sqlite converter.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import sqlite3
+import csv_to_sqlite
+
+#Todo:
+#.csv to .sqlite
+def convertToSqlite():
+    csv_name = "data.csv"
+    options = csv_to_sqlite.CsvOptions(typing_style="full", encoding="windows-1250")
+    input_files = [csv_name]  # pass in a list of CSV files
+    csv_to_sqlite.write_csv(input_files, "output.sqlite", options)
 
 if __name__ == '__main__':
     print('Welcome to SQLiteHelper!')
